b557abc302c9f4b9c8804eeab59b1f20d50be2a8
范文华 <531362022@qq.com>
2020-09-10 23:54:53 +0800

    refactor: Rename CustomPromise to PromiseListenCatch

--- index.ts
+++ index.ts
@@ -1,28 +1,28 @@
-export class CustomPromise<T = any> implements PromiseLike<T> {
+export class PromiseListenCatch<T = any> implements PromiseLike<T> {
   private readonly promise: Promise<T>;
-  private readonly previous: CustomPromise<any>[];
+  private readonly previous: PromiseListenCatch<any>[];
   private catchHandled: boolean = false;
 
   constructor(executor: ConstructorParameters<typeof Promise>[0]);
-  constructor(promise: Promise<T>, previous?: CustomPromise<any>[]);
+  constructor(promise: Promise<T>, previous?: PromiseListenCatch<any>[]);
 
-  constructor(executor: ConstructorParameters<typeof Promise>[0] | Promise<T>, previous?: CustomPromise<any>[]) {
+  constructor(executor: ConstructorParameters<typeof Promise>[0] | Promise<T>, previous?: PromiseListenCatch<any>[]) {
     this.promise = typeof executor === 'function' ? new Promise<T>(executor as any) : executor;
     this.previous = previous || [];
   }
 
-  then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): CustomPromise<TResult1 | TResult2> {
+  then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): PromiseListenCatch<TResult1 | TResult2> {
     const newPromise = this.promise.then<TResult1, TResult2>(onfulfilled, onrejected);
     this.setCatchHandled(!!onrejected);
 
-    return new CustomPromise<TResult1 | TResult2>(newPromise, this.previous.concat(this));
+    return new PromiseListenCatch<TResult1 | TResult2>(newPromise, this.previous.concat(this));
   }
 
-  catch<TResult = never>(onrejected?: ((reason: any) => TResult | PromiseLike<TResult>) | undefined | null): CustomPromise<T | TResult> {
+  catch<TResult = never>(onrejected?: ((reason: any) => TResult | PromiseLike<TResult>) | undefined | null): PromiseListenCatch<T | TResult> {
     const newPromise = this.promise.catch(onrejected);
     this.setCatchHandled(true);
 
-    return new CustomPromise<T | TResult>(newPromise, this.previous.concat(this));
+    return new PromiseListenCatch<T | TResult>(newPromise, this.previous.concat(this));
   }
 
   isCatched(): boolean {
