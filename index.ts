9501f549ca06f4f662a453e25789f36ff2191942
范文华 <531362022@qq.com>
2020-09-11 11:30:41 +0800

    feat: Add method appendCatchToEnd

--- index.ts
+++ index.ts
@@ -1,51 +1,102 @@
 export default class PromiseListenCatch<T = any> implements PromiseLike<T> {
   private readonly promise: Promise<T>;
-  private readonly previous: PromiseListenCatch<any>[];
+  protected prev: PromiseListenCatch<any> | null = null;
+  protected next: PromiseListenCatch<any>[] = [];
   private catchHandled: boolean = false;
+  private thenHandled: boolean = false;
 
   constructor(executor: ConstructorParameters<typeof Promise>[0]);
-  constructor(promise: Promise<T>, previous?: PromiseListenCatch<any>[]);
-
-  constructor(executor: ConstructorParameters<typeof Promise>[0] | Promise<T>, previous?: PromiseListenCatch<any>[]) {
+  constructor(promise: Promise<T>);
+  constructor(executor: ConstructorParameters<typeof Promise>[0] | Promise<T>) {
     this.promise = typeof executor === 'function' ? new Promise<T>(executor as any) : executor;
-    this.previous = previous || [];
   }
 
   then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): PromiseListenCatch<TResult1 | TResult2> {
     const newPromise = this.promise.then<TResult1, TResult2>(onfulfilled, onrejected);
+    this.setThenHandled(!!onfulfilled);
     this.setCatchHandled(!!onrejected);
 
-    return new PromiseListenCatch<TResult1 | TResult2>(newPromise, this.previous.concat(this));
+    return this.createNext(newPromise);
   }
 
   catch<TResult = never>(onrejected?: ((reason: any) => TResult | PromiseLike<TResult>) | undefined | null): PromiseListenCatch<T | TResult> {
     const newPromise = this.promise.catch(onrejected);
     this.setCatchHandled(true);
 
-    return new PromiseListenCatch<T | TResult>(newPromise, this.previous.concat(this));
+    return this.createNext(newPromise);
   }
 
   finally(onfinally?: (() => void) | undefined | null): PromiseListenCatch<T> {
     const newPromise = this.promise.finally(onfinally);
 
-    return new PromiseListenCatch(newPromise, this.previous.concat(this));
+    return this.createNext(newPromise);
   }
 
-  isCatched(): boolean {
+  hasThen(): boolean {
+    return this.thenHandled;
+  }
+
+  hasCatch(): boolean {
     return this.catchHandled;
+  }
+
+  /**
+   * @param {string | undefined} logLevel Default: non-log
+   */
+  appendCatchToEnd(logLevel?: 'log' | 'info' | 'warn' | 'error'): void {
+    let last: PromiseListenCatch = this;
+
+    while (true) {
+      const length = last.next.length;
+
+      if (!length) {
+        break;
+      } else if (length === 1) {
+        last = last.next[0];
+      } else {
+        last.next.forEach((item) => {
+          this.appendCatchToEnd.call(item, logLevel);
+        });
+        return;
+      }
+    }
+
+    last.catch((logLevel ? console[logLevel] : Function.prototype) as any);
   }
 
   protected toString() {
     return '[object PromiseListenCatch]';
   }
 
+  protected createNext<T1>(promise: Promise<T1>) {
+    const instance = new PromiseListenCatch<T1>(promise);
+    this.next.push(instance);
+    instance.prev = this;
+
+    return instance;
+  }
+
+  protected setThenHandled(is: boolean) {
+    this.thenHandled = is;
+
+    if (is) {
+      let prev: PromiseListenCatch | null = this;
+
+      while((prev = prev.prev) !== null) {
+        prev.thenHandled = true;
+      }
+    }
+  }
+
   protected setCatchHandled(is: boolean) {
     this.catchHandled = is;
 
     if (is) {
-      this.previous.forEach((item) => {
-        item.catchHandled = true;
-      });
+      let prev: PromiseListenCatch | null = this;
+
+      while((prev = prev.prev) !== null) {
+        prev.catchHandled = true;
+      }
     }
   }
 }
---
+++ test.ts
@@ -0,0 +1,128 @@
+import PromiseListenCatch from './index';
+
+process.addListener('unhandledRejection', console.log);
+
+(() => {
+  const promise = new PromiseListenCatch((resolve) => {
+    resolve();
+  });
+
+  promise.then(() => {
+    console.log('resolve1');
+  });
+})();
+
+(() => {
+  const promise = new PromiseListenCatch((_, reject) => {
+    reject('normal rejection');
+  });
+
+  promise.then(() => {
+    console.log('resolve1');
+  }).catch(console.log);
+})();
+
+
+(() => {
+  const promise = new PromiseListenCatch((_, reject) => {
+    reject('without catch');
+  });
+
+  // Throw
+  promise.then(() => {
+    console.log(1);
+  });
+
+  // Throw
+  promise.then(() => {
+    console.log(2);
+  });
+})();
+
+(() => {
+  const promise = new PromiseListenCatch((_, reject) => {
+    setTimeout(() => {
+      promise.appendCatchToEnd();
+      reject('with catch');
+    });
+  });
+
+  // Safe
+  promise.then(() => {
+    console.log(1);
+  });
+
+  // Safe
+  promise.then(() => {
+    console.log(2);
+  });
+})();
+
+
+(() => {
+  const promise = new PromiseListenCatch((resolve, reject) => {
+    setTimeout(() => {
+      if (promise.hasThen()) {
+        promise.hasCatch() || promise.appendCatchToEnd();
+        reject('with catch');
+      } else {
+        resolve();
+      }
+    });
+  });
+
+  // Safe
+  promise.then(() => {
+    console.log(1);
+  });
+})();
+
+(() => {
+  const promise = new PromiseListenCatch((resolve, reject) => {
+    setTimeout(() => {
+      if (promise.hasThen() && !promise.hasCatch()) {
+        promise.appendCatchToEnd();
+      }
+
+      if (promise.hasCatch()) {
+        reject('with catch');
+      } else {
+        resolve();
+      }
+    });
+  });
+
+  promise.catch(console.log);
+})();
+
+Promise.all([
+  new PromiseListenCatch((resolve) => {
+    resolve(1);
+  }),
+  new PromiseListenCatch((resolve) => {
+    setTimeout(() => {
+      resolve(2);
+    }, 200);
+  }),
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(3);
+    }, 100);
+  }),
+]).then(console.log);
+
+Promise.race([
+  new PromiseListenCatch((resolve) => {
+    resolve('race1');
+  }),
+  new PromiseListenCatch((resolve) => {
+    setTimeout(() => {
+      resolve('race2');
+    }, 200);
+  }),
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('race3');
+    }, 100);
+  }),
+]).then(console.log);
